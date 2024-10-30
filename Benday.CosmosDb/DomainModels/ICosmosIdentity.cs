6adda3f7456483b00eeede2a420b405e5f3b056e
Benjamin Day <benday@benday.com>
2024-10-30 08:39:37 -0400

    Adding code documentation

--- Benday.CosmosDb/DomainModels/ICosmosIdentity.cs
+++ Benday.CosmosDb/DomainModels/ICosmosIdentity.cs
@@ -5,21 +5,39 @@ namespace Benday.CosmosDb.DomainModels;
 using Benday.CosmosDb.Utilities;
 using Newtonsoft.Json;
 
+/// <summary>
+/// Represents the basic properties of a Cosmos DB entity.
+/// </summary>
 public interface ICosmosIdentity
 {
+    /// <summary>
+    /// Id of the entity.
+    /// </summary>
     [JsonProperty("id")]
     string Id { get; set; }
 
+    /// <summary>
+    /// Top-level partition key of the entity.
+    /// </summary>
     [JsonProperty("pk")]
     string PartitionKey { get; set; }
 
+    /// <summary>
+    /// Timestamp of the entity.
+    /// </summary>
     [JsonProperty("_ts")]
     [JsonConverter(typeof(UnixDateTimeConverter))]
     public DateTime Timestamp { get; set; }
 
+    /// <summary>
+    /// Optimistic concurrency token for the entity.
+    /// </summary>
     [JsonProperty("_etag")]
     public string Etag { get; set; }
 
+    /// <summary>
+    /// Second-level partition key for the entity.  This value describes the domain model type for the entity.
+    /// </summary>
     [JsonProperty(CosmosDbConstants.DiscriminatorPropertyName)]
     string DiscriminatorValue
     {
